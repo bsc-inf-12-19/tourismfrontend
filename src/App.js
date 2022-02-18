a37554a127e797f22edebf27147e1ef0a95e440e
bsc-inf-12-19 <bsc-inf-12-19@unima.ac.mw>
2022-02-17 18:21:45 -0800

    fixing home.js file and index.html file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './pages/home';
+import Gallery from './pages/gallery';
 import logo from './logo.svg';
 import './App.css';
 
 function App(){
   return <Home/>;
+  return <Gallery/>;
 }
 
 export default App;
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,33 +1,26 @@
 import React from 'react';
 
-class Home extends React.Component{
+class Gallery extends React.Component{
   render(){
      return( <div>
-
-        <h1><img class ="img1" src = "assets/images/b.jpg"/>MALAWI TOURISM</h1>
-         <br/>
-        <form>
+         <h1>Welcome to MalawiTourism gallery</h1>
+        
+          <form>
             <li><input  class = "id" type = "search" placeholder = "Search..."/></li>
             <br/>
             <li><input class = "id1" type = "submit" value = "Search"/></li>
             <br/>
         </form>        
-        <li><a href = "Home.html">HOME</a></li>
-        
+        <li><a href = "home.js">HOME</a></li>
         <li><a href = "#">DESTINATION</a></li>
         <li><a href = "gallery.js">GALLERY</a></li>
         <li><a href = "mailto:patrickkamphulusa@gmail.com">FEEDBACK</a></li>
-       
-        <div>
         <form class = "sign">
         <h2>Malawi Tourism</h2>
         <br/>
     <a href = "SignUp.html"><input type = "button" value = "SignUp"/></a>
     <a href = "#"><input type = "button" value = "SignIn"/></a>
-    </form>
-    </div>
-
-
+</form>
 
     </div>
      );
@@ -35,4 +28,4 @@ class Home extends React.Component{
   }  
 }
 
-export default Home;  
+export default Gallery;  
