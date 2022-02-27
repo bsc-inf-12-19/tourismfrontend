5db2f6626e1930c58e9c11d6653c092f7192313d
bsc-inf-12-19 <bsc-inf-12-19@unima.ac.mw>
2022-02-27 00:10:15 -0800

    removing unnecessaries from files

--- src/pages/feedback.js
+++ src/pages/feedback.js
@@ -3,23 +3,8 @@ import "./Feedback.css";
 class Feedback extends React.Component{
   render(){
      return(
-
     <div>
-
-{/* <body> */}
 <h1>Feedback</h1>
-    {/* <nav> */}
-        {/* <ul>
-                <form>
-                    <li><input  className = "id" type = "search" placeholder = "Search..."/></li>
-                    <li><input className = "id1" type = "submit" value = "Search"/></li>
-                </form> */}
-        {/* <li><button ><a href = "Home.html">HOME</a></button></li>
-        <li><button ><a href = "Destination.html">DESTINATION</a></button></li>
-        <li><button ><a href = "gallery.html">GALLERY</a></button></li>
-        <li><button ><a href = "Feedback.html">FEEDBACK</a></button></li> */}
-    {/* </ul> */}
-    {/* </nav> */}
     <h2><a href = "mailto:patrickkamphulusa@gmail.com">click to email</a></h2> 
     <div className="sign">
     <form>
@@ -32,7 +17,6 @@ class Feedback extends React.Component{
    <input className="c3" type = "submit" placeholder="submit"/> 
 </form>
 </div>
-{/* </body> */}
     </div> 
   );
          
--- src/pages/home.js
+++ src/pages/home.js
@@ -10,28 +10,15 @@ class Home extends React.Component{
             <li><input class = "id1" type = "submit" value = "Search"/></li>
             <br/>
         </form>
-       {/* <body> */}
-        
-        {/* <h1><img className ="img1" src = "/assets/images/b.jpg"/>WELCOME TO MALAWI TOURISM</h1> */}
-         <br/>
-       
-        {/* <li><button><a href = "home.js">HOME</a></button></li>
-    <li><button ><a href = "destination.js">DESTINATION</a></button></li>
-    <li><button><a href = "gallery.js">GALLERY</a></button></li>
-    <li><button ><a href = "feedback.js">FEEDBACK</a></button></li> */}
-{/* </body> */}
 <br/>
 <br/>
 <br/>
-      <h2>Experience the wonders of malawi nature</h2>
+      <h2>Experience the beauty of malawi nature</h2>
+      <marquee><h>Visit different tourist site within malawi and experience the beauty of malawi nature, including its massive lake Malawi, the mulanje mountain, zomba plateua, liwonde national park, and many other different amazing places within malawi. For more check out the gallery and destination sections on the top page</h></marquee>
+      <img src = "/assets/images/ga3.jpg" alt="Mulanje mountain"/>
+      <img src = "/assets/images/ga5.jpg" alt="Mulanje mountain"/>
+      <img src = "/assets/images/ga11.jpg" alt="Mulanje mountain"/>
       <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <marquee><h>Visit different tourist site within malawi and experience the beauty of malawi nature, including its massive lake Malawi, the mulanje mountain, zomba plateua, liwonde national park, and many other different amazing palces within malawi. For more check out the gallery and destination sections</h></marquee>
     
     </div>
      );
