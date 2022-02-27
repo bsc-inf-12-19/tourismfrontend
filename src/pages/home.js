f1101b5f3b8f6a58733c5fc25591983fe6451e5c
bsc-inf-12-19 <bsc-inf-12-19@unima.ac.mw>
2022-02-26 16:49:35 -0800

    editing home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,11 +4,11 @@ class Home extends React.Component{
   render(){
      return( <div>
        <body>
-        <h1><img className ="img1" src = "/assets/images/b.jpg"/>MALAWI TOURISM</h1>
+        <h1>WELCOME TO MALAWI TOURISM</h1>
+        {/* <h1><img className ="img1" src = "/assets/images/b.jpg"/>WELCOME TO MALAWI TOURISM</h1> */}
          <br/>
         <form>
             <li><input  className = "id" type = "search" placeholder = "Search..."/></li>
-            <br/>
             <li><input class = "id1" type = "submit" value = "Search"/></li>
             <br/>
         </form>
@@ -17,8 +17,11 @@ class Home extends React.Component{
     <li><button><a href = "gallery.js">GALLERY</a></button></li>
     <li><button ><a href = "feedback.js">FEEDBACK</a></button></li> */}
 </body>
-
-      <h2>Experience the wonders of nature</h2>
+<br/>
+<br/>
+<br/>
+      <h2>Experience the wonders of malawi nature</h2>
+      
     </div>
      );
          
