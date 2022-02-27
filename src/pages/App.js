e649e15ea554e514eadffcf93b0683df8a4773ea
bsc-inf-12-19 <bsc-inf-12-19@unima.ac.mw>
2022-02-26 17:43:26 -0800

    route added

--- src/pages/App.js
+++ src/pages/App.js
@@ -25,7 +25,7 @@ function App(){
         <Routes>
 
           <Route path="/" element={<Home/>}/>
-          {/* <Route path='/Home' element={<Home/>}/> */}
+          <Route path='/Home' element={<Home/>}/>
           <Route path='/Destination' element={<Destination/>}/>
           <Route path='/Gallery' element={<Gallery/>}/>
           <Route path='/Feedback' element={<Feedback/>}/>
